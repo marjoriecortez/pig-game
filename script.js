aa8a945f4c6b18ca012285a5611b747c7635d28b
Marjorie <marjorie.crtz@yahoo.com>
2024-01-30 19:49:44 +0800

    Add random dice roll functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Declaration
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const rollBtn = document.querySelector('.btn--roll');
+const diceEl = document.querySelector('.dice');
+
+// Initial Values
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Switch Player Function
+const switchPlayer = function () {};
+
+rollBtn.addEventListener('click', function () {
+  // Random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
